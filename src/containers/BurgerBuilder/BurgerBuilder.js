21deb05cb151e3cf4d801568c2b90ea0544e55d3
Dwi Darma <dwikbtn15@gmail.com>
2020-11-25 14:20:00 +0800

    Adding state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
+import Burger from '../../components/Burger/Burger'
+
 export default class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 1,
+            meat: 2
+        }
+    }
+
     render() {
         return (
             <>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controllers</div>
             </>
         )
