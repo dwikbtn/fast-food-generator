d26d87f45afc1ba9db386a430fd3aa143c9d0f89
Dwi Darma <dwikbtn15@gmail.com>
2020-12-14 17:06:40 +0800

    adding logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const Layout = (props) => (
     <>
+        <Toolbar />
         <div>Toolbaar, SideDrawer, Backdrop</div>
         <main className={classes.content}>
             {props.children}
