1141ba8454e650246f158aff6dc2b2a6c5cb16c1
Dwi Darma <dwikbtn15@gmail.com>
2021-01-02 14:21:00 +0800

    adding post at axios

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
-
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-order';
 const INGREDIENTS_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -80,7 +80,26 @@ export default class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You Continue!')
+        // alert('You Continue!')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Max',
+                address: {
+                    street: 'SomeWhere',
+                    zipCode: '21312',
+                    country: 'ID'
+                },
+                email: 'dad@gmail.com',
+            },
+            deliveryMethod: 'fastest'
+        }
+
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(err => console.log(err))
+
     }
 
     render() {
